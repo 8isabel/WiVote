e5d1d4b8fc3ad34f3e61824db4757813057d898a
isabel <isabeldeheer8@gmail.com>
2026-09-18 12:29:07 +0200

    style

--- index.php
+++ index.php
@@ -1,13 +1,2 @@
 <?php
-require 'config/con.php';
-
-try{
-    $query = "SELECT * FROM `login`";
-    $stmt = $conn->prepare($query);
-    $stmt->execute();
-
-    include 'views/index_view.php';
-}catch(PDOException $e){
-    echo $e->getMessage();
-    exit;
-}
+include 'views/index_view.php';
