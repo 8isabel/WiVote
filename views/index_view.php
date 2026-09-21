860bd72016aa6ca8fd824624ba1fd01d2a74bacf
isabel <isabeldeheer8@gmail.com>
2026-09-21 09:42:36 +0200

    styling update

--- views/index_view.php
+++ views/index_view.php
@@ -8,9 +8,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Turret Road' rel='stylesheet'>
+    <link href="https://fonts.cdnfonts.com/css/porter-sans-block" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-
+<div class="title">
+    <h1>Wivote</h1>
+</div>
+<div class="choices">
+    <div class="choice"></div>
+    <div class="choice"></div>
+    <div class="choice"></div>
+    <div class="choice"></div>
+    <div class="choice"></div>
+    <div class="choice"></div>
+</div>
+<header></header>
 </body>
 </html>
